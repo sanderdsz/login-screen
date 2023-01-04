e3fa9f8ca10be207751285fadee2d5673008abb3
Sander <sanderdsz@gmail.com>
2023-01-04 08:18:59 -0300

    chore: dashboard barebone

---
+++ pages/dashboard.tsx
@@ -0,0 +1,14 @@
+import SideLogo from "../components/SideLogo";
+
+export default function Dashboard() {
+  return (
+    <div className='h-screen flex flex-row'>
+      <div className='w-auto flex justify-center'>
+        <SideLogo/>
+      </div>
+      <div className='w-full flex justify-center items-center bg-slate-100'>
+
+      </div>
+    </div>
+  )
+}
